1e6e731f9d7b9ff6915f5069a6f29edadd444447
vandana <vandana19@navgurukul.org>
2022-11-04 14:39:30 +0530

    chat application by using socket.io

---
+++ server.js
@@ -0,0 +1,31 @@
+const { Socket } = require("dgram");
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+
+const PORT = process.env.PORT || 8000
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get("/", (req, res) => {
+    // console.log("hello world")
+    // res.send("hello world")
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+// socket
+const io = require("socket.io")(http)
+io.on("connection", (socket) => {
+    console.log("connected......")
+    // listen
+    socket.on("message", (msg) => {
+        // console.log(msg)
+        socket.broadcast.emit("message", msg)
+    })
+})
